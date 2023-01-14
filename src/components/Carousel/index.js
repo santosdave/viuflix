568b116a477709231f06e1191970b88c9d9cc981
santosdave <51959957+santosdave@users.noreply.github.com>
2023-01-14 13:17:29 +0300

    minor changes

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,9 +1,62 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Navigation } from "swiper";
 
-function carousel() {
+import styles from "./styles.module.scss";
+
+
+import Card from "../Card";
+import FavoriteButton from "../../UI/FavoriteButton";
+import RenderIf from "../../utils/renderIf";
+
+
+const breakpoints = {
+  640: {
+    slidesPerView: 3,
+  },
+  768: {
+    slidesPerView: 4,
+  },
+  1024: {
+    slidesPerView: 5,
+  },
+  1440: {
+    slidesPerView: 6,
+  },
+  1700: {
+    slidesPerView: 7,
+  },
+};
+
+function Carousel({ title, data, isCast, style }) {
   return (
-    <div>carousel</div>
+    <div className={styles.carousel} style={style}>
+      <RenderIf isTrue={title}>
+        <div className={styles.title_wrapper}>
+          <div className={styles.title}>{title}</div>
+        </div>
+      </RenderIf>
+      <Swiper
+        className='mySwiper'
+        freemode={true}
+        navigation={true}
+        slidesPerView={3}
+        spaceBetween={10}
+        breakpoints={breakpoints}
+        modules={[Navigation, FreeMode]}
+      >
+
+        {data?.map((card) => {
+          const { id, title, poster_path, profile_path, name, character } =
+            card;
+          const path = isCast ? profile_path : poster_path;
+          const link = isCast ? `/person/${id}` : `/movie/${id}`;
+        })}
+
+      </Swiper>
+    </div>
   )
 }
 
-export default carousel
+export default Carousel
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import NowPlaying from '../../components/NowPlaying';
 import Layout from "../../UI/Layout";
+import Loading from "../../UI/Loading";
+import useFetch from "../../hooks/useFetch";
+import { API_KEY } from "../../utils/constants";
+import Carousel from '../../components/Carousel';
+
+
+function Popular() {
+  const url = `movie/popular?api_key=${API_KEY}&language=en-US`;
+  const { data, isLoading } = useFetch(url);
+
+  return (
+    <Loading loading={isLoading}>
+      <Carousel
+        title="Popular Movies"
+        style={{ marginTop: 40 }}
+      />
+    </Loading>
+  );
+}
 
 function Home() {
   return (
     <Layout title="Home">
-      <NowPlaying/>
+      <NowPlaying />
+      <Popular />
       <div className="footer">Build on React • 2021</div>
     </Layout>
   )
