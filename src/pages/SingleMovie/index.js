aa9c79751ccceaf3927b6c97182d2bb34d6314fd
santosdave <51959957+santosdave@users.noreply.github.com>
2023-01-21 14:42:15 +0300

    minor changes

--- src/pages/SingleMovie/index.js
+++ src/pages/SingleMovie/index.js
@@ -1,4 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import Layout from '../../UI/Layout';
+import Loading from "../../UI/Loading";
+import { API_KEY } from '../../utils/constants'
+import Carousel from '../../components/Carousel';
+import RenderIf from '../../utils/renderIf';
+import useFetch from '../../hooks/useFetch';
+
+
+
+
+
 
 function SingleMovie() {
   return (
